16269b0815c399afc044c13a5013834c408cbc1f
rendisurya <rendisurya@users.noreply.github.com>
2026-09-04 01:07:05 +0700

    update: status_need_validate.sql query

--- tools/queries/status_need_validate.sql
+++ tools/queries/status_need_validate.sql
@@ -3,18 +3,4 @@ FROM `mp_data` a
 LEFT JOIN mp_remark b on a.order_number = b.order_number
 WHERE `validation_status` = '2'
 ORDER BY a.`order_date` ASC
-LIMIT 10;
-
-UPDATE mp_data
-SET validation_status = '1',
-    validation_is_need_validated = '0'
-WHERE order_number IN (
-    SELECT order_number FROM (
-        SELECT order_number
-        FROM mp_data
-        WHERE validation_status = '2' 
-          AND validation_is_need_validated = '1'
-        ORDER BY order_date ASC
-        LIMIT 1
-    ) AS tmp
-);
+LIMIT 10
